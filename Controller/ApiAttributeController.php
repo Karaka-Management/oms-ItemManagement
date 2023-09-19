8fb0bd17394726809262fc7e620d390211c2e881
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-19 03:51:45 +0000

    fix double log for default attribute value changes

--- Controller/ApiAttributeController.php
+++ Controller/ApiAttributeController.php
@@ -231,7 +231,9 @@ final class ApiAttributeController extends Controller
 
         $this->updateModel($request->header->account, $old, $new, ItemAttributeMapper::class, 'item_attribute', $request->getOrigin());
 
-        if ($new->value->getValue() !== $old->value->getValue()) {
+        if ($new->value->getValue() !== $old->value->getValue()
+            && $new->type->custom
+        ) {
             $this->updateModel($request->header->account, $old->value, $new->value, ItemAttributeValueMapper::class, 'attribute_value', $request->getOrigin());
         }
 
