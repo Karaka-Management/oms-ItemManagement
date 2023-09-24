edb6619eb54426de97c737eea0b969386f0229a5
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-24 02:48:07 +0000

    fix tests

--- Controller/ApiAttributeController.php
+++ Controller/ApiAttributeController.php
@@ -451,17 +451,17 @@ final class ApiAttributeController extends Controller
         // @todo: I don't think values can be deleted? Only Attributes
         // However, It should be possible to remove UNUSED default values
         // either here or other function?
-        if (!empty($val = $this->validateAttributeValueDelete($request))) {
-            $response->header->status = RequestStatusCode::R_400;
-            $this->createInvalidDeleteResponse($request, $response, $val);
+        // if (!empty($val = $this->validateAttributeValueDelete($request))) {
+        //     $response->header->status = RequestStatusCode::R_400;
+        //     $this->createInvalidDeleteResponse($request, $response, $val);
 
-            return;
-        }
+        //     return;
+        // }
 
-        /** @var \Modules\ItemManagement\Models\ItemAttributeValue $itemAttributeValue */
-        $itemAttributeValue = ItemAttributeValueMapper::get()->where('id', (int) $request->getData('id'))->execute();
-        $this->deleteModel($request->header->account, $itemAttributeValue, ItemAttributeValueMapper::class, 'item_attribute_value', $request->getOrigin());
-        $this->createStandardDeleteResponse($request, $response, $itemAttributeValue);
+        // /** @var \Modules\ItemManagement\Models\ItemAttributeValue $itemAttributeValue */
+        // $itemAttributeValue = ItemAttributeValueMapper::get()->where('id', (int) $request->getData('id'))->execute();
+        // $this->deleteModel($request->header->account, $itemAttributeValue, ItemAttributeValueMapper::class, 'item_attribute_value', $request->getOrigin());
+        // $this->createStandardDeleteResponse($request, $response, $itemAttributeValue);
     }
 
     /**
