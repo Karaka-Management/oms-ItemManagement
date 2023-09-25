1f7a0e6f1e9cdc18d41b07d43ec23251aec0d465
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 15:14:05 +0000

    fix tests

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -687,6 +687,12 @@ final class ApiController extends Controller
             }
         }
 
+        if (empty($uploaded)) {
+            $this->createInvalidAddResponse($request, $response, []);
+
+            return;
+        }
+
         $this->createModelRelation(
             $request->header->account,
             (int) $request->getData('item'),
