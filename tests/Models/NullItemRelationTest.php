f221f72ab7e7419103a1f455614a10f11cfc11c2
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:44:00 +0000

    Remove getId() function call from null models

--- tests/Models/NullItemRelationTest.php
+++ tests/Models/NullItemRelationTest.php
@@ -37,7 +37,7 @@ final class NullItemRelationTest extends \PHPUnit\Framework\TestCase
     public function testId() : void
     {
         $null = new NullItemRelation(2);
-        self::assertEquals(2, $null->getId());
+        self::assertEquals(2, $null->id);
     }
 
     /**
--- tests/Models/NullItemRelationTypeTest.php
+++ tests/Models/NullItemRelationTypeTest.php
@@ -37,7 +37,7 @@ final class NullItemRelationTypeTest extends \PHPUnit\Framework\TestCase
     public function testId() : void
     {
         $null = new NullItemRelationType(2);
-        self::assertEquals(2, $null->getId());
+        self::assertEquals(2, $null->id);
     }
 
     /**
